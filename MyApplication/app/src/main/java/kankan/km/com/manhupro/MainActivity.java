76279eb95f2294e557689442f40f589c8a429c8a
fdong1 <fdong1@yahoo.com>
2016-02-06 14:06:38 +0800

    tab

--- MyApplication/app/src/main/java/kankan/km/com/manhupro/MainActivity.java
+++ MyApplication/app/src/main/java/kankan/km/com/manhupro/MainActivity.java
@@ -1,27 +1,30 @@
 package kankan.km.com.manhupro;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        System.out.print("MainActivity :: onCreate");
 
-        System.out.print("11111111");
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
