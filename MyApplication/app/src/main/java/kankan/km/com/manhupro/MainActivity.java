edb3792eee8b64cafbd83531652dfb6b7c1a5f23
fdong1 <fdong1@yahoo.com>
2016-02-06 10:24:48 +0800

    测试提交

--- MyApplication/app/src/main/java/kankan/km/com/manhupro/MainActivity.java
+++ MyApplication/app/src/main/java/kankan/km/com/manhupro/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        System.out.print("11111111");
     }
 
     @Override
