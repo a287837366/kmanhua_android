2be4739f18f251baeb7d70d261b7ffaf2f50908f
fdong1 <fdong1@yahoo.com>
2016-05-11 16:48:37 +0800

    no message

---
+++ MyApplication/app/src/main/java/kankan/km/com/manhupro/main/activity/CreateManhuaAcvitiy.java
@@ -0,0 +1,21 @@
+package kankan.km.com.manhupro.main.activity;
+
+import android.os.Bundle;
+
+import kankan.km.com.manhupro.BaseAcvitiy;
+import kankan.km.com.manhupro.R;
+
+/**
+ * Created by apple on 16/5/11.
+ */
+public class CreateManhuaAcvitiy extends BaseAcvitiy{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createmanhua);
+
+    }
+}
