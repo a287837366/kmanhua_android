e96d63290d840f9a292c8756aadb20639f9e4b8e
fdong1 <fdong1@yahoo.com>
2016-05-13 17:26:17 +0800

    相册操作开始

--- MyApplication/app/src/main/java/kankan/km/com/manhupro/main/activity/CreateManhuaAcvitiy.java
+++ MyApplication/app/src/main/java/kankan/km/com/manhupro/main/activity/CreateManhuaAcvitiy.java
@@ -1,18 +1,18 @@
 package kankan.km.com.manhupro.main.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import kankan.km.com.manhupro.BaseAcvitiy;
 import kankan.km.com.manhupro.R;
 import kankan.km.com.manhupro.tools.tools.AlbumTools.AlbumHelper;
+import kankan.km.com.manhupro.tools.tools.AlbumTools.acvitity.ChoosePictureActivity;
 
 /**
  * Created by apple on 16/5/11.
  */
-public class CreateManhuaAcvitiy extends BaseAcvitiy{
-
-    AlbumHelper helper;
-
+public class CreateManhuaAcvitiy extends BaseAcvitiy implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +22,45 @@ public class CreateManhuaAcvitiy extends BaseAcvitiy{
 
 
         initData();
+        initView();
+
     }
 
     private void initData(){
-        helper = AlbumHelper.getHelper();
-        helper.init(this);
 
-        helper.getImagesBucketList(false);
+
+    }
+
+    private void initView(){
+
+        findViewById(R.id.button).setOnClickListener(this);
+
+    }
+
+    private void gotoChooseImageView(){
+        Intent intent = new Intent();
+        intent.setClass(this, ChoosePictureActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
+        switch (view.getId()){
+
+            case R.id.button:
+
+                gotoChooseImageView();
+
+                break;
+
+
+            default:
+
+                break;
+
+        }
 
     }
 }
---
+++ MyApplication/app/src/main/java/kankan/km/com/manhupro/tools/tools/AlbumTools/acvitity/ChoosePictureActivity.java
@@ -0,0 +1,36 @@
+package kankan.km.com.manhupro.tools.tools.AlbumTools.acvitity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import kankan.km.com.manhupro.R;
+import kankan.km.com.manhupro.tools.tools.AlbumTools.AlbumHelper;
+
+/**
+ * Created by apple on 16/5/13.
+ */
+public class ChoosePictureActivity extends Activity{
+
+    AlbumHelper helper;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choosepicture);
+
+
+        initData();
+        initView();
+
+    }
+
+    private void initData(){
+
+    }
+
+    private void initView(){
+
+
+    }
+}
