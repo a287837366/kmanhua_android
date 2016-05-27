7414053cf35faa974375f4b4e081e736c3a12395
fdong1 <fdong1@yahoo.com>
2016-05-27 09:38:45 +0800

    导航页添加

---
+++ MyApplication/app/src/main/java/kankan/km/com/manhupro/splash/SplashAcvitiy.java
@@ -0,0 +1,22 @@
+package kankan.km.com.manhupro.splash;
+
+import android.os.Bundle;
+
+import kankan.km.com.manhupro.BaseAcvitiy;
+import kankan.km.com.manhupro.R;
+
+/**
+ * Created by apple on 16/5/27.
+ */
+public class SplashAcvitiy extends BaseAcvitiy{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acvitiy_splash);
+
+    }
+
+
+
+}
