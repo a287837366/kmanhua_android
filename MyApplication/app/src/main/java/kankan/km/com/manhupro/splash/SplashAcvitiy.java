652200fdbd007ed5c12e4650fb3978879f677299
fdong1 <fdong1@yahoo.com>
2016-05-27 17:36:50 +0800

    导航页 修改

--- MyApplication/app/src/main/java/kankan/km/com/manhupro/splash/SplashAcvitiy.java
+++ MyApplication/app/src/main/java/kankan/km/com/manhupro/splash/SplashAcvitiy.java
@@ -1,22 +1,52 @@
 package kankan.km.com.manhupro.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import kankan.km.com.manhupro.BaseAcvitiy;
+import kankan.km.com.manhupro.MainActivity;
 import kankan.km.com.manhupro.R;
+import kankan.km.com.manhupro.splash.service.SplashService;
 
 /**
  * Created by apple on 16/5/27.
  */
 public class SplashAcvitiy extends BaseAcvitiy{
 
+
+    private SplashService service;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acvitiy_splash);
 
+
+        initData();
+        initView();
+
+        service.checkAppVersion("");
     }
 
+    private void initData(){
+        service = new SplashService(this, null);
+
+    }
+
+    private void initView(){
+
+
+    }
+
+
+    private void gotoMainActivity(){
+        Intent intent = new Intent();
+        intent.setClass(this, MainActivity.class);
+        startActivity(intent);
+
+        finish();
+
+    }
 
 
 }
---
+++ MyApplication/app/src/main/java/kankan/km/com/manhupro/splash/service/SplashService.java
@@ -0,0 +1,57 @@
+package kankan.km.com.manhupro.splash.service;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+
+import kankan.km.com.manhupro.tools.httptools.HttpClinet;
+import kankan.km.com.manhupro.tools.httptools.ResponseCallback;
+
+
+/**
+ * Created by apple on 16/5/27.
+ */
+public class SplashService implements ResponseCallback {
+
+    private RequestQueue mQueue;
+    private Handler mHandler;
+
+
+
+    public SplashService(Activity activity, Handler handler){
+
+        mQueue = Volley.newRequestQueue(activity);
+        mHandler = handler;
+    }
+
+    public void checkAppVersion(String appVersion){
+
+        mQueue.add(HttpClinet.getInstance().getRequset("/kankanAdmin/CheckVersionUpdate", this, 0));
+
+    }
+
+
+
+    @Override
+    public void send(int method, int tag, String json) {
+
+
+
+    }
+
+    @Override
+    public void error(int tag, int error) {
+        Log.d(">>>>>>>","error");
+
+        Message msg = new Message();
+        msg.what = 999;
+        mHandler.sendMessage(msg);
+
+    }
+}
